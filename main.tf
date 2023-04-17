861739ee12e5db8b36c92515a7b2b63ffc2a32fd
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-17 23:20:29 +0530

    updated

--- main.tf
+++ main.tf
@@ -40,20 +40,18 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   for_each = var.public_subnets
-
-
   tags = merge(
     var.tags,
     {
       Name = "${var.env}-${each.value["name"]}"
     }
   )
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
 }
 
 
