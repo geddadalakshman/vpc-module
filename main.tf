027809ebe3dcdbf9aadcb1021ea83f049051614c
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-17 22:40:06 +0530

    updated

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_route_table" "public_route" {
     }
   )
   route {
-    cidr_block = each.value["cidr_block"]
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
