5b089591da0540b98f9271728165b521e98ffc31
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-19 12:56:36 +0530

    updated

--- main.tf
+++ main.tf
@@ -39,6 +39,22 @@ resource "aws_internet_gateway" "igw" {
   )
 }
 
+##NatGateway
+#resource "aws_nat_gateway" "nat_gw" {
+#  allocation_id = aws_eip.example.id
+#  subnet_id     = aws_subnet.example.id
+#
+#  # To ensure proper ordering, it is recommended to add an explicit dependency
+#  # on the Internet Gateway for the VPC.
+#  depends_on = [aws_internet_gateway.example]
+#}
+#
+####Elastic IP
+#resource "aws_eip" "lb" {
+#  instance = aws_instance.web.id
+#  vpc      = true
+#}
+
 ###---public route tables
 resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.main.id
